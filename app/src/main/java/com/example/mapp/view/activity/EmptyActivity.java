9e39a0c497fd67c5b82197e39cc59f728bae5603
munakbey <munakbey@gmail.com>
2020-02-16 23:24:46 +0300

    kk

---
+++ app/src/main/java/com/example/mapp/view/activity/EmptyActivity.java
@@ -0,0 +1,16 @@
+package com.example.mapp.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mapp.R;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+    }
+}
